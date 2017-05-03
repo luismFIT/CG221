9260e51cc7a50ebf6fc50512d5b895e7b739c9b2
luismFIT <luis_montalvo2@fitnyc.edu>
2017-05-03 18:13:44 -0400

    Add files via upload

--- p5/sketch.js
+++ p5/sketch.js
@@ -4,9 +4,23 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-    fill(100);
-  } else {
     fill(200);
+    star(mouseX, mouseY, 10, 30, 5);
   }
-  ellipse(mouseX, mouseY, 30, 30);
+
+}
+
+function star(x, y, radius1, radius2, npoints) {
+  var angle = TWO_PI / npoints;
+  var halfAngle = angle/2.0;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius2;
+    var sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a+halfAngle) * radius1;
+    sy = y + sin(a+halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
